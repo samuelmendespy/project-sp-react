2fffdbdb7e4e64a37ee3fd468085f49ed858595d
Samuel Mendes <75790706+samuelmendespy@users.noreply.github.com>
2023-09-05 19:02:45 -0300

    feat: project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,179 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import { 
+  AiFillWindows,
+  AiFillApple,
+  AiFillAndroid
+} from 'react-icons/ai';
+import Image from "next/image";
+import avatar from "../public/avatar.png";
+import cloud from "../public/cloud.png";
+import database from "../public/database.png";
+import server from "../public/server.png";
+import art from "../public/art.png";
+import {useState} from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false); 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={darkMode ? 'dark': ''}>
+      <Head>
+      <title>Project</title>
+        <meta name="description" content="next project" />
+        <link></link>
+      </Head>
+
+
+    <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+      <section className="min-h-screen">
+        <nav className='py-10 mb-12 flex justify-between dark:text-white'>
+        <h1 className='text-xl'>Project</h1>
+        <ul className='flex items-center'>
+          <li>
+            <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl' />
+          </li>
+          <li>
+            <a className='bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 rounded-xl ml-8'
+            href="#">
+              Resume
+            </a>
+          </li>
+        </ul>
+        </nav>
+        <div className='text-center p-10'>
+          <h2 className='text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl'>
+            Project
+          </h2>
+          <h3 className='text-2xl py-2 dark:text-white md:text-3xl'>
+            Details and information
+          </h3>
+          <p className='text-md py-5 leading-8 text-gray-800 dark:text-gray-200 md:text-xl max-w-lg mx-auto'>
+            Lorem ipsum Lorem ipsumLorem
+            Lorem ipsumLorem ipsumLorem ipsum ipsumLorem 
+            ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem 
+            Lorem ipsumLorem ipsumLorem ipsumipsumLorem ipsumLorem ipsum</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className='text-5xl flex justify-center gap-16 py-3 dark:text-gray-400 text-gray-600'>
+          <AiFillAndroid />
+          <AiFillWindows />
+          <AiFillApple />
+        </div>
+        <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
+          <Image src={avatar} alt={'Avatar image'} layout='fill' objectFit='cover' />
+        </div>
+      </section>
+      <section>
+        <div>
+          <h3 className='text-3xl py-1 dark:text-white'>
+            Features
+          </h3>
+          <p className='text-md py-2 leading-8 dark:text-gray-200 text-gray-800'>
+          Lorem IpsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem 
+          ipsumLorem ipsum ipsumLorem <span className='text-teal-500'>IpSum</span> ipsumLorem ipsum ipsumLorem 
+          ipsumLorem ipsum ipsumLorem ipsumLorem lorem ipsum  ipsumLorem ipsumLorem lorem ipsum ipsumLorem ipsumLorem lorem ipsum ipsumLorem ipsumLorem lorem ipsum <span className='text-teal-500'>lOREM</span> ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem 
+          ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem 
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className='text-md py-2 leading-8 dark:text-gray-200 text-gray-800'>
+            Lorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUMLorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUMLorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUMLorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUMLorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUMLorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUM
           </p>
-        </a>
+        </div>
+        <div className='lg:flex gap-10'>
+          <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
+            <Image src={cloud} alt='Image' width={100} height={100} className='mx-auto' />
+            <h3 className='text-lg font-medium pt-8 pb-2'>Cloud</h3>
+            <p className='py-2'>
+              ipsumLorem ipsumLorem ipsum ipsumLoremipsumLorem ipsumLorem ipsum ipsumLorem
+            </p>
+            <h4 className='py-4 text-teal-600'>Cloud features</h4>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+          <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
+            <Image src={database} alt='Image' width={100} height={100} className='mx-auto' />
+            <h3 className='text-lg font-medium pt-8 pb-2'>Database</h3>
+            <p className='py-2'>
+              ipsumLorem ipsumLorem ipsum ipsumLoremipsumLorem ipsumLorem ipsum ipsumLorem
+            </p>
+            <h4 className='py-4 text-teal-600'>Cloud features</h4>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
+            <Image src={server} alt='Image' width={100} height={100} className='mx-auto' />
+            <h3 className='text-lg font-medium pt-8 pb-2'>Storage</h3>
+            <p className='py-2'>
+              ipsumLorem ipsumLorem ipsum ipsumLoremipsumLorem ipsumLorem ipsum ipsumLorem
+            </p>
+            <h4 className='py-4 text-teal-600'>Cloud features</h4>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+            <p className='text-gray-800 py-1'>ipsumLorem lorem Ipsum</p>
+          </div>
+        </div>
+      </section>
+
+
+      <section>
+        <div>
+
+          <h3 className='text-3xl py-1 dark:text-white' >
+            Technologies
+          </h3>
+
+          <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+          Lorem IpsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem 
+          ipsumLorem ipsum ipsumLorem <span className='text-teal-500'>IpSum</span> ipsumLorem ipsum ipsumLorem 
+          ipsumLorem ipsum ipsumLorem ipsumLorem lorem ipsum  ipsumLorem ipsumLorem lorem ipsum ipsumLorem ipsumLorem lorem ipsum ipsumLorem ipsumLorem lorem ipsum <span className='text-teal-500'>lOREM</span> ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem 
+          ipsumLorem ipsum ipsumLorem ipsumLorem ipsum ipsumLorem 
           </p>
-        </a>
-      </div>
+          <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+            Lorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUMLorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUMLorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUMLorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUMLorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUMLorem ipsum Lorem Ipsum LoreM IpsUm Lorem IPsUM
+          </p>
+
+        </div>
+        <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
+
+          <div className='basis-1/3 flex-1'>
+            <Image src={art} alt='' className='rounded-lg object-cover' 
+            layout='responsive'/>
+          </div>
+
+          <div className='basis-1/3 flex-1'>
+            <Image src={art} alt='' className='rounded-lg object-cover' 
+            layout='responsive'
+            />
+          </div>
+          <div className='basis-1/3 flex-1'>
+            <Image src={art} alt='' className='rounded-lg object-cover' 
+             layout='responsive'/>
+          </div>
+
+
+          <div className='basis-1/3 flex-1'>
+            <Image src={art} alt='' className='rounded-lg object-cover' 
+            layout='responsive'/>
+          </div>
+          <div className='basis-1/3 flex-1'>
+            <Image src={art} alt='' className='rounded-lg object-cover' 
+            layout='responsive'/>
+          </div>
+          <div className='basis-1/3 flex-1'>
+            <Image src={art} alt='' className='rounded-lg object-cover' 
+            layout='responsive'/>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+    
+    </div>
+    )
 }
